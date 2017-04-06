9c5adce1ade4cfcd86b8a95252d0c0a59f0d47df
datanasov <dido@omisoft.eu>
2017-04-06 16:39:27 +0300

    Added some error handling

---
+++ src/main/java/com/omisoft/server/common/constants/CommonConstants.java
@@ -0,0 +1,10 @@
+package com.omisoft.server.common.constants;
+
+/**
+ * Holds common constants
+ * Created by dido on 06.04.17.
+ */
+public class CommonConstants {
+  public static final String AUTHORIZATION_HEADER = "authorization";
+  public static final String LOGGED_USER = "LOGGED_USER";
+}
---
+++ src/main/java/com/omisoft/server/common/filters/AuthorityFilter.java
@@ -0,0 +1,96 @@
+package com.omisoft.server.common.filters;
+
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.omisoft.server.common.auth.AuthUtils;
+import com.omisoft.server.common.auth.LoggedUserInfo;
+import com.omisoft.server.common.auth.UserAuthority;
+import com.omisoft.server.common.constants.CommonConstants;
+import com.omisoft.server.common.exceptions.SecurityException;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.joda.time.DateTime;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import javax.servlet.*;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.ParseException;
+
+import static com.omisoft.server.common.constants.CommonConstants.AUTHORIZATION_HEADER;
+
+
+/**
+ * Authorization filter for REST services.
+ * Works with JWT header
+ * Created by nslavov on 3/25/16.
+ */
+@Singleton
+@Slf4j
+public class AuthorityFilter implements Filter {
+
+
+  @Inject
+  private UserAuthority authority;
+
+  @Override
+  public void init(FilterConfig filterConfig) throws ServletException {
+
+  }
+
+  // TODO Refactor
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
+      throws IOException, ServletException {
+
+    HttpServletRequest httpRequest = (HttpServletRequest) request;
+    HttpServletResponse httpResponse = (HttpServletResponse) response;
+    String authHeader = httpRequest.getHeader(AUTHORIZATION_HEADER);
+    log.info(authHeader);
+    String method = httpRequest.getMethod();
+    Cookie[] cokkies = httpRequest.getCookies();
+    Cookie authCookie;
+    if (StringUtils.isBlank(authHeader) || authHeader.equals("null")) {
+      for (Cookie c : cokkies) {
+        if (AUTHORIZATION_HEADER.equalsIgnoreCase(c.getName())) {
+          authCookie = c;
+          authHeader = authCookie.getValue();
+          break;
+        }
+      }
+    }
+
+    if (method.equals("OPTIONS")) {
+      httpResponse.setStatus(200);
+    } else {
+      if ((StringUtils.isBlank(authHeader) || !authority.isExist(authHeader))) {
+        httpResponse.setStatus(401);
+      } else {
+        JWTClaimsSet claimSet;
+        try {
+          claimSet = (JWTClaimsSet) AuthUtils.decodeToken(authHeader);
+        } catch (ParseException | JOSEException | SecurityException e) {
+          httpResponse.setStatus(401);
+          return;
+        }
+        // ensure that the token is not expired
+        if (new DateTime(claimSet.getExpirationTime()).isBefore(DateTime.now())) {
+          httpResponse.setStatus(401);
+        } else {
+          LoggedUserInfo redisDTO = authority.getUser(authHeader);
+          request.setAttribute(CommonConstants.LOGGED_USER, redisDTO);
+          filterChain.doFilter(request, response);
+        }
+      }
+    }
+
+  }
+
+  @Override
+  public void destroy() {
+
+  }
+}
